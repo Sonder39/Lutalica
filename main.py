ff09b9a7cbd1e48b9e4862df89619d56de719a3c
Sonder39 <CyberAnts@example.com>
2024-05-03 18:12:07 +0800

    设计靶机扫描窗口UI

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import logging
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont, QIcon
-from PyQt6.QtWidgets import (QApplication, QVBoxLayout, QFrame)
-from qfluentwidgets import FluentWindow, FluentIcon, setFont, CaptionLabel
+from PyQt6.QtWidgets import QApplication, QFrame, QHBoxLayout, QVBoxLayout, QWidget
+from qfluentwidgets import FluentWindow, FluentIcon, ImageLabel, SubtitleLabel, setFont, CaptionLabel, TextEdit, \
+    LineEdit, PushButton, ToolButton
 
 logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
 
@@ -19,15 +20,111 @@ class HomeWindow(QFrame):
         self.setLayout(layout)
 
 
+class subWindow1(QFrame):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        layout = QHBoxLayout(self)
+        leftLayout = QVBoxLayout()
+        rightLayout = QVBoxLayout(self)
+
+        cover = 'resource/Archive1.png'
+        self.image = ImageLabel(cover, self)
+        self.image.scaledToHeight(290)
+        self.image.setBorderRadius(8, 8, 8, 8)
+        leftLayout.addWidget(self.image)
+
+        subtitleLabel = '靶机扫描'
+        self.subtitle = SubtitleLabel(subtitleLabel, self)
+        self.subtitle.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        setFont(self.subtitle, 24)
+        self.subtitle.setFixedHeight(50)
+        leftLayout.addWidget(self.subtitle, 1, Qt.AlignmentFlag.AlignLeft)
+
+        scanContainer = QWidget()
+        targetLayout = QHBoxLayout(scanContainer)
+        self.targetLabel = CaptionLabel("扫描目标", self)
+        setFont(self.targetLabel, 18)
+        self.icon = ToolButton(self)
+        self.icon.setIcon(FluentIcon.CONNECT)
+        self.target = LineEdit()
+        self.target.setPlaceholderText("https://192-168-1-{X}.pvp3994.bugku.cn")
+        buttonText = "开始扫描"
+        self.scan = PushButton(buttonText, self)
+        targetLayout.addWidget(self.targetLabel)
+        targetLayout.addWidget(self.icon)
+        targetLayout.addWidget(self.target)
+        targetLayout.addWidget(self.scan)
+        leftLayout.addWidget(scanContainer)
+
+        paramContainer = QWidget()
+        paramLayout = QHBoxLayout(paramContainer)
+        self.startLabel = CaptionLabel("起始值", self)
+        setFont(self.startLabel, 18)
+        self.start = LineEdit()
+        self.start.setFixedWidth(100)
+        self.start.setPlaceholderText("起始值")
+        paramLayout.addWidget(self.startLabel)
+        paramLayout.addWidget(self.start)
+        # startLayout.addStretch(1)
+        self.endLabel = CaptionLabel("最大值", self)
+        setFont(self.endLabel, 18)
+        self.end = LineEdit()
+        self.end.setFixedWidth(100)
+        self.end.setPlaceholderText("最大值")
+        paramLayout.addWidget(self.endLabel)
+        paramLayout.addWidget(self.end)
+        # startLayout.addStretch(1)
+        self.stepLabel = CaptionLabel("步进", self)
+        setFont(self.stepLabel, 18)
+        self.step = LineEdit()
+        self.step.setFixedWidth(100)
+        self.step.setPlaceholderText("步进")
+        paramLayout.addWidget(self.stepLabel)
+        paramLayout.addWidget(self.step)
+        self.ignoreLabel = CaptionLabel("忽略值", self)
+        setFont(self.ignoreLabel, 18)
+        self.ignore = LineEdit()
+        self.ignore.setFixedWidth(120)
+        self.ignore.setPlaceholderText("忽略值[1, 39...]")
+        paramLayout.addWidget(self.ignoreLabel)
+        paramLayout.addWidget(self.ignore)
+        paramLayout.addStretch(1)
+        leftLayout.addWidget(paramContainer)
+
+        self.listLabel = CaptionLabel("[+]靶机列表", self)
+        setFont(self.listLabel, 18)
+        leftLayout.addWidget(self.listLabel)
+        self.list = TextEdit()
+        self.list.setReadOnly(True)
+        leftLayout.addWidget(self.list)
+        leftLayout.setStretchFactor(self.list, 1)
+
+        self.logLabel = CaptionLabel("[*]扫描日志", self)
+        setFont(self.logLabel, 18)
+        rightLayout.addWidget(self.logLabel)
+        self.log = TextEdit()
+        self.log.setReadOnly(True)
+        rightLayout.addWidget(self.log)
+
+        layout.addLayout(leftLayout)
+        layout.addLayout(rightLayout)
+
+
 class App(FluentWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Lutalica")
         self.setWindowIcon(QIcon('resource/logo.png'))
         self.showMaximized()
+
         self.home = HomeWindow()
         self.home.setObjectName("Home")
         self.addSubInterface(self.home, FluentIcon.HOME, "主页")
+
+        self.window1 = subWindow1()
+        self.window1.setObjectName("Window1")
+        self.addSubInterface(self.window1, FluentIcon.HISTORY, "作业检查")
 
 
 def run():
