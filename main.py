6a621727672d42aefc1a24f7ff31f84d34117c4d
Sonder39 <CyberAnts@example.com>
2024-05-03 15:38:57 +0800

    添加Home页

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import logging
 
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont, QIcon
-from PyQt6.QtWidgets import (QApplication)
-from qfluentwidgets import FluentWindow
+from PyQt6.QtWidgets import (QApplication, QVBoxLayout, QFrame)
+from qfluentwidgets import FluentWindow, FluentIcon, setFont, CaptionLabel
 
 logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
+
+
+class HomeWindow(QFrame):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        layout = QVBoxLayout(self)
+        self.label = CaptionLabel("Lutalica", self)
+        setFont(self.label, 64)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self.label, 1, Qt.AlignmentFlag.AlignCenter)
+        self.setLayout(layout)
 
 
 class App(FluentWindow):
@@ -12,6 +24,9 @@ class App(FluentWindow):
         super().__init__()
         self.setWindowTitle("Lutalica")
         self.setWindowIcon(QIcon('resource/logo.png'))
+        self.home = HomeWindow()
+        self.home.setObjectName("Home")
+        self.addSubInterface(self.home, FluentIcon.HOME, "主页")
 
 
 def run():
